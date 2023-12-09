e3be5e773dcf55d452fb8b276a64863492f88715
Steven Joseph <stevolisisjoseph@gmail.com>
2023-12-09 17:05:55 +0100

    trying apk convert and build

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './app/index.js'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app 2023!</Text>
       <StatusBar style="auto" />
+      <Home/>
     </View>
   );
 }
